52cb28a7a23d1e0d37a5c5dffd5741bcd5ca20a3
selmi khaled <selmikhaled25@gmail.com>
2020-11-17 00:03:08 +0100

    authentification

--- model/user.model.js
+++ model/user.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const personne = require('./personne.model');
 const user = new Schema({
+  password:{
+    type:String,
+    required:true 
+  },
     givenQuantity:{
       type:Number,
       default: 0
@@ -11,6 +15,7 @@ const user = new Schema({
       default: 0
     },
     annonces:[{type: mongoose.Schema.Types.ObjectId, ref: "Annonces"}],
+    
 });
 
 const User = personne.discriminator('User',user);
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const {body}=require('express-validator/check');
 const UserCatController=require('../Controllers/userController');
+const User = require('../model/user.model');
 
-
+router.put('/signup',[ 
+    body('email').isEmail().withMessage('Please enter a valid').custom((value,{req})=>{
+        return User.findOne({email:value}).then(userDoc =>{
+            if (userDoc){
+                return Promise.reject('E-Mail address already exists!');
+            }
+        });
+    })
+    .normalizeEmail(),
+    body('password')
+    .trim().isLength({ min:5 }),
+    body('nom')
+    .trim()
+    .not()
+    .isEmpty()
+],UserCatController.signup );
+router.post('/login',UserCatController.login);
 router.get('/', UserCatController.getAllUsers);
 
 router.post('/add',UserCatController.addUser);

a2dfa1bb42891657793d467487f58b8eabed94d2
selmi khaled <selmikhaled25@gmail.com>
2020-12-30 18:53:02 +0100

    reglage update user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,14 +1,14 @@
-const User = require('../model/user.model');
-const { validationResult } = require('express-validator/check');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
+const User = require("../model/user.model");
+const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    user: 'don.project2020@gmail.com',
-    pass: 'Mern@123',
+    user: "don.project2020@gmail.com",
+    pass: "Mern@123",
   },
 });
 
@@ -16,7 +16,7 @@ exports.signup = async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const error = new Error('Validation failed.');
+      const error = new Error("Validation failed.");
       error.statusCode = 422;
       error.data = errors.array();
       throw error;
@@ -24,7 +24,7 @@ exports.signup = async (req, res, next) => {
     const { nom, prenom, tel, email, password } = req.body;
     const userDoc = await User.findOne({ email: email });
     if (userDoc) {
-      return res.status(404).json({ message: 'email déja existe' });
+      return res.status(404).json({ message: "email déja existe" });
     }
     const hashedpw = await bcrypt.hash(password, 12);
     if (hashedpw) {
@@ -37,24 +37,24 @@ exports.signup = async (req, res, next) => {
       });
       const addedUser = await user.save();
 
-      res.status(200).json({ message: 'User created', userId: addedUser._id });
+      res.status(200).json({ message: "User created", userId: addedUser._id });
       transporter.sendMail(
         {
-          from: 'youremail@gmail.com',
+          from: "youremail@gmail.com",
           to: email,
-          subject: 'Sending Email using Node.js',
-          text: 'hello ' + nom + ' welcom to Najemn3awen',
+          subject: "Sending Email using Node.js",
+          text: "hello " + nom + " welcom to Najemn3awen",
         },
         function (error, info) {
           if (error) {
             console.log(error);
           } else {
-            console.log('Email sent: ' + info.response);
+            console.log("Email sent: " + info.response);
           }
         }
       );
     } else {
-      return res.status(400).json({ message: 'mot de passe pas hashé' });
+      return res.status(400).json({ message: "mot de passe pas hashé" });
     }
   } catch (err) {
     return res.status(400).json({ err });
@@ -67,7 +67,7 @@ exports.login = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: email });
     if (!user) {
-      return res.status(401).json({ message: 'vous avez pas un compte!' });
+      return res.status(401).json({ message: "vous avez pas un compte!" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
@@ -80,15 +80,18 @@ exports.login = async (req, res, next) => {
         nom,
         prenom,
         tel,
+        grade: "user",
       };
 
-      const token = await jwt.sign(payload, 'don2020!', { expiresIn: 3600 });
+      const token = await jwt.sign(payload, "don2020!", { expiresIn: 3600 });
 
       if (token) {
-        res.status(200).json({ success: true, token: 'Bearer ' + token ,UserId:_id });
+        res
+          .status(200)
+          .json({ success: true, token: "Bearer " + token, UserId: _id });
       }
     } else {
-      return res.status(400).json({ message: 'mot de passe incorrect' });
+      return res.status(400).json({ message: "mot de passe incorrect" });
     }
   } catch (err) {
     return res.status(400).json({ err });
@@ -97,7 +100,7 @@ exports.login = async (req, res, next) => {
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const Users = await User.find().populate('annonces');
+    const Users = await User.find().populate("annonces");
     res.status(200).json({ Users });
   } catch (err) {
     res.status(400).json({ err });
@@ -109,7 +112,7 @@ exports.FindUserById = async (req, res) => {
     const admin = await User.findById(req.params.id);
     res.status(200).json(admin);
   } catch (err) {
-    res.status(400).json('Error: ' + err);
+    res.status(400).json("Error: " + err);
   }
 };
 
@@ -118,24 +121,25 @@ exports.deleteUser = async (req, res) => {
   const { userId } = req.userData;
   try {
     await User.findByIdAndDelete(userId);
-    res.status(200).json({ message: 'user deleted.' });
+    res.status(200).json({ message: "user deleted." });
   } catch (err) {
-    res.status(400).json('Error: ' + err);
+    res.status(400).json("Error: " + err);
   }
 };
 
 exports.UpDateUser = async (req, res) => {
   const { userId } = req.userData;
-
-  const updatedUser = req.body;
+  const { nom, prenom, tel, email } = req.body;
+  const updatedUser = { nom, prenom, tel, email, grade: "user" };
   try {
-    const user = await User.findByIdAndUpdate(
-      userId,
-      { $set: updatedUser },
-      { new: true }
-    );
-    if (user) res.status(200).json({ message: 'User updated!', user });
-    else throw new Error('User Undefined !');
+    await User.findByIdAndUpdate(userId, { $set: updatedUser }, { new: true });
+
+    const token = await jwt.sign(updatedUser, "don2020!", {
+      expiresIn: 3600,
+    });
+    res
+      .status(200)
+      .json({ message: "User updated!", token: "Bearer " + token });
   } catch (err) {
     res.status(400).json({ Error: err.message });
   }

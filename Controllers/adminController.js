2e659c0a660495a5c1ca639037d24426a41eb56e
selmi khaled <selmikhaled25@gmail.com>
2021-01-29 14:34:02 +0100

    refacto login-admin-user in one method

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,31 +1,35 @@
-const router = require('express').Router();
-const Admin = require('../model/admin.model');
+const Admin = require("../model/admin.model");
+const bcrypt = require("bcryptjs");
 
 exports.getAllAdmins = async (req, res) => {
   try {
     const admins = await Admin.find();
     res.status(200).json(admins);
   } catch (err) {
-    res.status(400).json('Error: ' + err);
+    res.status(400).json("Error: " + err);
   }
 };
 
 exports.addAdmin = async (req, res) => {
-  const { nom, prenom, tel, email, grade } = req.body;
-
-  const newadmin = new Admin({
-    nom,
-    prenom,
-    tel: Number(tel),
-    email,
-    grade,
-  });
+  const { nom, prenom, tel, email, password, grade } = req.body;
+  const hashedpw = await bcrypt.hash(password, 12);
   try {
-    const admin = await newadmin.save();
-    if(!admin){
-      throw new Error("cannot add admin !")
+    if (hashedpw) {
+      const newadmin = new Admin({
+        nom,
+        prenom,
+        tel: Number(tel),
+        email,
+        password: hashedpw,
+        grade,
+      });
+
+      const admin = await newadmin.save();
+      if (!admin) {
+        throw new Error("cannot add admin !");
+      }
+      res.status(200).json({ message: "Admin added", admin });
     }
-    res.status(200).json({ message: 'Admin added', admin });
   } catch (err) {
     res.status(400).json({ Error: err.message });
   }
@@ -36,16 +40,16 @@ exports.RechercheAdminbyId = async (req, res) => {
     const admin = await Admin.findById(req.params.id);
     res.status(200).json(admin);
   } catch (err) {
-    res.status(400).json('Error: ' + err);
+    res.status(400).json("Error: " + err);
   }
 };
 
 exports.delteAdmin = async (req, res) => {
   try {
     await Admin.findByIdAndDelete(req.params.id);
-    res.status(200).json('admin deleted.');
+    res.status(200).json("admin deleted.");
   } catch (err) {
-    res.status(400).json('Error: ' + err);
+    res.status(400).json("Error: " + err);
   }
 };
 
@@ -61,7 +65,7 @@ exports.UpDateAdmin = async (req, res) => {
       { new: true }
     );
 
-    res.status(200).json({ message: 'admin updated!', admin });
+    res.status(200).json({ message: "admin updated!", admin });
   } catch (err) {
     res.status(400).json({ Error: err.message });
   }
